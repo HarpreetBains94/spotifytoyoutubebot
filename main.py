a32a6f1da324bcabb767ff8714763f0dfc858207
Harpreet Bains <hazbains20@gmail.com>
2021-03-21 20:28:35 +0000

    "added" apple music support

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from spotify_api import SpotifyAPI
 from youtube_api import YoutubeAPI
 from keep_alive import keep_alive
+import time
 
 client = discord.Client()
 
@@ -18,18 +19,33 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  if message.author == os.getenv('STEE'):
-    await message.channel.send('You suck')
   content_arr = message.content.split(' ')
-  matching_substr = None
+  matching_substr_spotify = None
+  matching_substr_apple = None
   for substr in content_arr:
     if 'https://open.spotify.com/track/' in substr:
-      matching_substr = substr
-  if matching_substr is None:
+      matching_substr_spotify = substr
+    if 'https://music.apple.com/' in substr:
+      matching_substr_apple = substr
+  if matching_substr_spotify is not None:
+    await do_youtube_match_for_spotify(matching_substr_spotify, message)
     return
-  await do_youtube_match(matching_substr, message)
+  if matching_substr_apple is not None:
+    """
+      Getting the title from the embed in the message for apple links because im not paying
+      for an apple developer licence for a dumb discord bot.
 
-async def do_youtube_match(url, message):
+      Hopefully this wont have any edge cases/risk conditions
+    """
+    await message.channel.send('Apple Sucks')
+    time.sleep(10)
+    channel = message.channel
+    new_message = await channel.fetch_message(message.id)
+    if new_message.embeds:
+      await do_youtube_match_for_apple(message.embeds[0].title, message)
+  
+
+async def do_youtube_match_for_spotify(url, message):
   try:
     track_data = spotifyWrapper.get_track_data(url)
   except:
@@ -38,6 +54,13 @@ async def do_youtube_match(url, message):
 
   try:
     search_query = track_data['title'] + ' - ' + ' '.join(track_data['artists'])
+    video_url = youtubeWrapper.get_youtube_video(search_query)
+    await message.channel.send(video_url)
+  except:
+    await message.channel.send('Unable to find Youtube match')
+
+async def do_youtube_match_for_apple(search_query, message):
+  try:
     video_url = youtubeWrapper.get_youtube_video(search_query)
     await message.channel.send(video_url)
   except:
